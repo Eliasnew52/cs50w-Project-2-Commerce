d8b9e391a24dfb5679b5b8cf0c86c2c17f6df7ba
Eliasnew52 <xxeliasnew52xx@gmail.com>
2024-01-29 12:59:12 -0600

    Starting Models and Supa User Done

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    CatName= models.CharField(max_length=40)
+
+
+class Listings(models.Model):
+    Title = models.CharField(max_length=50)
+    Description = models.CharField(max_length=300)
+    Image = CloudinaryField('image')
+    Price = models.FloatField()
+    IsActive = models.BooleanField(default = True)
+    Owner = models.ForeignKey(User,on_delete=models.CASCADE, blank = True, null = True, related_name = 'user')
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE, blank = True, related_name = 'category')
